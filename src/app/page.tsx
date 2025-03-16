5b1304afc8ef78a01812a30d2425ccb14a924cae
Robert Grzonka <r.grzonka@gmail.com>
2025-03-16 22:43:05 +0100

    Add LOGIN and REGISTER links to root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 import ApolloWrapper from '@/lib/ApolloWrapper'
-import Loader from '@/components/Loader'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-6">
+    <div className="min-h-screen flex flex-col items-center justify-center p-6 gap-y-3">
       <ApolloWrapper>
-        <Loader />
+        <h1 className="text-4xl font-light">
+          🧞‍♀️ evo<span className="text-green-500 font-extrabold">Flow</span>™
+        </h1>
+        <div className="text-center">
+          <Link
+            href="/auth/signin"
+            className="text-green-500 hover:text-green-600 font-semibold transition"
+          >
+            LOG IN{' '}
+          </Link>
+          or{' '}
+          <Link
+            href="/auth/signup"
+            className="text-green-500 hover:text-green-600 font-semibold transition"
+          >
+            REGISTER
+          </Link>
+        </div>
       </ApolloWrapper>
     </div>
   )
